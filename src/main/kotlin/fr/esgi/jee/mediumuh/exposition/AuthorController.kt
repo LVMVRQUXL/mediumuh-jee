8dcf10355440e9c0041e2b20c7e84e0a36ecff31
Antonin Texier <antonin.texier@gmail.com>
2021-04-29 09:24:50 +0200

    feat(exposition): Create method endpoint to save a new author.

--- src/main/kotlin/fr/esgi/jee/mediumuh/exposition/AuthorController.kt
+++ src/main/kotlin/fr/esgi/jee/mediumuh/exposition/AuthorController.kt
@@ -1,15 +1,27 @@
 package fr.esgi.jee.mediumuh.exposition
 
 import fr.esgi.jee.mediumuh.domain.author.Author
+import fr.esgi.jee.mediumuh.domain.author.AuthorAlreadyExistsException
 import fr.esgi.jee.mediumuh.domain.author.AuthorService
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+import java.net.URI
 
 @RestController
 @RequestMapping("/api/authors")
-class AuthorController(val authorService: AuthorService) {
+data class AuthorController(val authorService: AuthorService) {
 
     @GetMapping
     fun findAll(): List<Author> = authorService.findAll()
+
+    @PostMapping
+    fun save(@RequestBody authorRequest: AuthorRequest): ResponseEntity<Author> {
+        return try {
+            val author = authorService.save(authorRequest)
+            ResponseEntity.created(URI.create("/api/authors/${author.id}")).build()
+        } catch (e: AuthorAlreadyExistsException) {
+            ResponseEntity.status(HttpStatus.CONFLICT).build()
+        }
+    }
 }
