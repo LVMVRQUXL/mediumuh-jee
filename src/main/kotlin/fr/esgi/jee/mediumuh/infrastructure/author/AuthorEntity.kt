204f157ab64b9c9752086bb38cdae0cd2798c9d0
Antonin Texier <antonin.texier@gmail.com>
2021-04-29 09:16:09 +0200

    feat(infrastructure): Add a new field for author entity.

--- src/main/kotlin/fr/esgi/jee/mediumuh/infrastructure/author/AuthorEntity.kt
+++ src/main/kotlin/fr/esgi/jee/mediumuh/infrastructure/author/AuthorEntity.kt
@@ -1,16 +1,22 @@
 package fr.esgi.jee.mediumuh.infrastructure.author
 
+import java.time.LocalDateTime
+import java.util.*
 import javax.persistence.*
 
 @Entity
+@Table(name = "author")
 data class AuthorEntity(
         @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        val id: String,
+        @GeneratedValue(strategy = GenerationType.AUTO)
+        val id: UUID,
 
         @Column(nullable = false)
         val firstName: String,
 
         @Column(nullable = false)
-        val lastName: String
+        val lastName: String,
+
+        @Column(nullable = false)
+        val creation: LocalDateTime = LocalDateTime.now()
 )
