42feb7dd2289f850babb5e1b3f07d62a3f927f1b
Antonin Texier <antonin.texier@gmail.com>
2021-04-29 09:24:18 +0200

    feat(domain): Create method to save a new author.

--- src/main/kotlin/fr/esgi/jee/mediumuh/domain/author/AuthorService.kt
+++ src/main/kotlin/fr/esgi/jee/mediumuh/domain/author/AuthorService.kt
@@ -1,12 +1,27 @@
 package fr.esgi.jee.mediumuh.domain.author
 
-import fr.esgi.jee.mediumuh.infrastructure.author.AuthorRepository
+import fr.esgi.jee.mediumuh.exposition.AuthorRequest
+import fr.esgi.jee.mediumuh.infrastructure.author.AuthorEntity
+import fr.esgi.jee.mediumuh.infrastructure.author.IAuthorRepository
 import org.springframework.stereotype.Service
+import java.util.*
 
 @Service
 data class AuthorService(val authorRepository: IAuthorRepository) {
     fun findAll(): List<Author> {
         val entities = authorRepository.findAll()
         return entities.map { entity -> AuthorMapper().from(entity) }
+    }
+
+    fun save(author: AuthorRequest): Author {
+        val authorEntity = AuthorEntity(UUID.randomUUID(), author.firstName, author.lastName)
+
+        if (authorRepository.existsById(authorEntity.id)) {
+            throw AuthorAlreadyExistsException()
+        }
+
+        val authorSaved = authorRepository.save(authorEntity)
+
+        return AuthorMapper().from(authorSaved)
     }
 }
